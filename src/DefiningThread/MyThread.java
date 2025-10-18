be440073e1c7808e5cc9a579ebc38c1b5862b330
Anil <anilkumargonuguntla03@gmail.com>
2025-10-18 10:04:21 +0530

    DefiningThread

---
+++ src/DefiningThread/MyThread.java
@@ -0,0 +1,20 @@
+package DefiningThread;
+
+public class MyThread extends Thread{
+    @Override
+    public void run() {
+        for(int i=0;i<10;i++){
+            System.out.println("child Thread"+Thread.currentThread().getName());
+        }
+    }
+}class Test{
+    public static void main(String[] args)  {
+        MyThread thread = new MyThread();
+        thread.start();
+        for(int i=0;i<10;i++){
+            System.out.println("Main Thread"+Thread.currentThread().getName());
+        }
+    }
+}
+
+
